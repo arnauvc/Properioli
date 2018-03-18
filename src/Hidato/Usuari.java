70ccccaf0829bf1e476eb9c83762b88c359f061f
rabbitwhile <vallsarnau19@gmail.com>
2018-03-18 18:00:24 +0100

    Usuari.java

---
+++ src/Hidato/Usuari.java
@@ -0,0 +1,14 @@
+package Hidato;
+
+public class Usuari {
+
+    private String nom;
+
+    public String GetNom(){
+        return nom;
+    }
+    public void SetNom(String nom){
+        this.nom = nom;
+    }
+
+}

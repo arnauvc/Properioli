7af20ead077e66935e95069eec8f1e08fa4aa82a
Magnusson19 <36761142+Magnusson19@users.noreply.github.com>
2018-04-03 12:04:40 +0200

    Add files via upload

--- src/Hidato/ComprovarHidato.java
+++ src/Hidato/ComprovarHidato.java
@@ -1,6 +1,6 @@
 package Hidato;
 
-import Hidato.Tauler;
+import javafx.scene.transform.MatrixType;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,8 +14,18 @@ public class ComprovarHidato {
    private String adjacencies;
    private String files;
    private String columnes;
+   private Vector<Vector<String>> Tauler;
 
-   public int ObrirFitxer (String nom) {//Nom ha de ser idHidato.txt
+    private static boolean isNumeric(String cadena){
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+    }
+
+   private int ObrirFitxer (String nom) {//Nom ha de ser idHidato.txt
        this.nom = nom;
        try {
            x = new Scanner(new File(nom));
@@ -25,7 +35,7 @@ public class ComprovarHidato {
        return 0;
    }
 
-   public int TencarFitxer(String nom) {
+   private int TencarFitxer(String nom) {
        if (nom != this.nom) {
            return 2;//error de tencament de fitxer
        }
@@ -35,11 +45,38 @@ public class ComprovarHidato {
        }
    }
 
-   /*public int ComprovarHidato (){
-        for (int i = 0; i < 4; i++) {
-
+   private int TransformarHidato(String nom){
+       if (ObrirFitxer(nom) != 0) return ObrirFitxer(nom);
+       Tauler = new Vector<Vector<String>>();
+       Tcela = x.next();x.next();
+       adjacencies = x.next();x.next();
+       files = x.next();x.next();
+       columnes = x.next();
+       String aux;
+       for (int i = 0; i < Integer.parseInt(files); i++) {
+           for (int j = 0; j < Integer.parseInt(columnes); j++) {
+               if (x.hasNext()) {
+                   aux = x.next();
+                   if (aux != ",") {
+                       Tauler.get(i).set(j, aux);
+                       if (1)
+                   }
+               }
+           }
        }
+       if (x.hasNext()) return 3; //Error no te mateixes files i columnes;
+       if (TencarFitxer(nom) != 0) return TencarFitxer(nom);
+       return 0;
    }
-   */
+
+   private int ComprovarAdjacencia (Vector<Vector<String>> matriu, int f, int c) {
+
+   }
+
+   public int ComprovarHidato (String nom) {
+       if (TransformarHidato(nom) != 0) return TransformarHidato(nom);
+
+       return 0;
+   }
 
 }

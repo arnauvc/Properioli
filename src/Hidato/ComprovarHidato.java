bd2e4cae01ace21d1f8c5ca7fead57ac97ce47a3
Magnusson19 <36761142+Magnusson19@users.noreply.github.com>
2018-04-04 20:23:28 +0200

    Add files via upload

--- src/Hidato/ComprovarHidato.java
+++ src/Hidato/ComprovarHidato.java
@@ -1,82 +1,128 @@
 package Hidato;
 
-import javafx.scene.transform.MatrixType;
+import javafx.util.Pair;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class ComprovarHidato {
 
    private Scanner x;
    private String nom;
-   private String Tcela;
+   private char Tcela;
    private String adjacencies;
-   private String files;
-   private String columnes;
-   private Vector<Vector<String>> Tauler;
+   private int files;
+   private int columnes;
+   private String[][] Tauler;
+   private ArrayList<Pair<Integer , Integer>> Numeros;
 
     private static boolean isNumeric(String cadena){
         try {
-            Integer.parseInt(cadena);
+            Integer.parseInt(String.valueOf(cadena));
             return true;
         } catch (NumberFormatException nfe){
             return false;
         }
     }
 
-   private int ObrirFitxer (String nom) {//Nom ha de ser idHidato.txt
+   private boolean ObrirFitxer (String nom) {//Nom ha de ser idHidato.txt
        this.nom = nom;
        try {
            x = new Scanner(new File(nom));
        } catch (FileNotFoundException e) {
-           return 1; //error al obrir el ditxer;
+           System.out.println("No ho trobo");
+           return false;
        }
-       return 0;
+       return true;
    }
 
-   private int TencarFitxer(String nom) {
+   private boolean TencarFitxer(String nom) {
        if (nom != this.nom) {
-           return 2;//error de tencament de fitxer
-       }
-       else {
+           return false;
+       } else {
            x.close();
-           return 0;
+           return true;
        }
    }
 
-   private int TransformarHidato(String nom){
-       if (ObrirFitxer(nom) != 0) return ObrirFitxer(nom);
-       Tauler = new Vector<Vector<String>>();
-       Tcela = x.next();x.next();
-       adjacencies = x.next();x.next();
-       files = x.next();x.next();
-       columnes = x.next();
-       String aux;
-       for (int i = 0; i < Integer.parseInt(files); i++) {
-           for (int j = 0; j < Integer.parseInt(columnes); j++) {
-               if (x.hasNext()) {
-                   aux = x.next();
-                   if (aux != ",") {
-                       Tauler.get(i).set(j, aux);
-                       if (1)
+   private boolean TransformarHidato(String nom){
+        Numeros = new ArrayList<Pair<Integer , Integer>>();
+       if (!ObrirFitxer(nom)) return false; //error al obrir el ditxer;
+       String line = null;
+       char aux;
+       line = x.next();
+       if (line.length() > 8 || line.length() < 7) return false;
+       else {
+           Tcela = line.charAt(0);
+           if (line.length() == 8) {
+               adjacencies = line.substring(2, 3);
+               files = Integer.parseInt(String.valueOf(line.charAt(5)));
+               columnes = (2*(Integer.parseInt(String.valueOf(line.charAt(7)))))-1;
+           }
+           else {
+               adjacencies = line.substring(2, 2);
+               files = Integer.parseInt(String.valueOf(line.charAt(4)));
+               columnes = (2*(Integer.parseInt(String.valueOf(line.charAt(6)))))-1;
+           }
+       }
+       Tauler = new String[Integer.parseInt(String.valueOf(files))][Integer.parseInt(String.valueOf(columnes))];
+       Pair<Integer, Integer> p;
+       for (int i = 0; i < files; i++) {
+           if (x.hasNext()) line = x.next();
+           for (int j = 0; j < columnes; j++) {
+               aux = line.charAt(j);
+               if (aux != ',') {
+                   Tauler[i][j] = String.valueOf(aux);
+                   if (isNumeric(String.valueOf(aux))) {
+                       p = new Pair<Integer, Integer>(i, j);
+                       Numeros.add(p);
                    }
                }
            }
        }
-       if (x.hasNext()) return 3; //Error no te mateixes files i columnes;
-       if (TencarFitxer(nom) != 0) return TencarFitxer(nom);
-       return 0;
+       if (x.hasNext()) return false; //Error no te mateixes files i columnes;
+       if (!TencarFitxer(nom)) return false;//error de tencament de fitxer
+       return true;
    }
 
-   private int ComprovarAdjacencia (Vector<Vector<String>> matriu, int f, int c) {
-
+   private boolean ComprovarAdjacencia (Integer f, Integer c, Integer valor) {
+        Integer numero = -1;
+       for (int i = f-1; i <= f+1; i++) {
+           for (int j = c-1; j <= c+1; j++) {
+               if (adjacencies.equals("C")) {
+                   if (i >= 0 && j >= 0 && i <= (files-1) && j <= (columnes-1)) {
+                       System.out.println("HOLAAAA");
+                       if (isNumeric(Tauler[i][j]) && (i != f || j != c)) {
+                           numero = Integer.parseInt(Tauler[i][j]);
+                           System.out.format("%d", numero);
+                           if ((valor % 2) == 0) {
+                               if ((numero % 2) == 0) return false;
+                           }
+                           else {
+                               if ((numero % 2) != 0) return false;
+                           }
+                       }
+                   }
+               }
+           }
+       }
+       return true;
    }
 
-   public int ComprovarHidato (String nom) {
-       if (TransformarHidato(nom) != 0) return TransformarHidato(nom);
+   private boolean ComprovarIgualtat (int i, Integer valor) {
+        if (i >= Numeros.size()) return true;
+        if (valor.equals(Integer.parseInt(Tauler[Numeros.get(i).getKey()][Numeros.get(i).getValue()]))) return false;
+        ComprovarIgualtat(i+1, valor);
+        return true;
+   }
 
-       return 0;
+   public boolean Comprovar (String nom) {
+       if (!TransformarHidato(nom)) return false;
+        for (int i = 0; i < Numeros.size(); i++) {
+            if (!(ComprovarIgualtat(i+1, Integer.parseInt(Tauler[Numeros.get(i).getKey()][Numeros.get(i).getValue()])))) return false;
+            if (!(ComprovarAdjacencia(Numeros.get(i).getKey(), Numeros.get(i).getValue(), Integer.parseInt(Tauler[Numeros.get(i).getKey()][Numeros.get(i).getValue()])))) return false;
+        }
+       return true;
    }
 
 }

50bc8f631cea518ed18c5d8d75742dad5e1d208d
rabbitwhile <vallsarnau19@gmail.com>
2018-04-17 11:41:09 +0200

    Set Tauler

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import Hidato.CtrlPresGestor;
-import Hidato.Driver_Generacio;
-
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Driver_Generacio dg = new Driver_Generacio();
-        dg.Test();
-        //CtrlPresGestor cpg = new CtrlPresGestor();
-        //cpg.Inicia();
+        CtrlPresGestor cpg = new CtrlPresGestor();
+        cpg.Inicia();
     }
 }

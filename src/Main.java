caac5feed1bf85e1a44f51de29894b583cc2392b
rabbitwhile <vallsarnau19@gmail.com>
2018-05-01 18:16:36 +0200

    Ajust parametres

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,7 @@ import Hidato.DriverPartida;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        /*Driver_Resolucio dr = new Driver_Resolucio();
-        dr.Prova();*/
-
         CtrlPresGestor cpg = new CtrlPresGestor();
         cpg.Inicia();
-
-        /*Test_HS test_hs = new Test_HS();
-        test_hs.Provar();*/
-
-        /*DriverPartida dp = new DriverPartida();
-        dp.Test();*/
-
     }
 }

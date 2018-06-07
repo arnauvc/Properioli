5363cb7a59c1c22b94a5be4fbac84a60014a51dc
TheCasul <marcman2008@hotmail.com>
2018-06-07 19:25:39 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import Hidato.DriverPresentacio;
+import Hidato.Part1.Inici;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		DriverPresentacio dp = new DriverPresentacio();
-		dp.Inici();
+		/*DriverPresentacio dp = new DriverPresentacio();
+		dp.Inici();*/
+		Inici.main(null);
 	}
 }
